fe7f0487443dc8bcd8ab60f45922645d5d1fd0e5
Gabriel <anzaldo.gabriel.dev@gmail.com>
2024-07-19 11:18:44 -0300

    Type added for product, inside global types.ts file.

--- reactExam/simple-product-page/src/app/products/page.tsx
+++ reactExam/simple-product-page/src/app/products/page.tsx
@@ -1,11 +1,12 @@
 import { fetchProducts } from "../../api/useApi";
+import { Product } from "../../types";
 
 export default async function Page() {
   const products = await fetchProducts();
   return (
     <main>
       <section>
-        {products?.map?.((item, index) => (
+        {products?.map?.((item: Product, index: number) => (
           <h2 key={item.id}>
             {index + 1} - {item.title}
           </h2>
